04c7173d9fcb2f63d70eed7ccd81e149ed669bcf
Minal Kharche <minal.berkeley@gmail.com>
2019-06-25 22:23:23 -0700

    updated with inquirer for asking user their character

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,3 +14,44 @@ CFonts.say('Welcome!', {
     space: true,                // define if the output text should have empty lines on top and on the bottom
     maxLength: '0',             // define how many character can be on one line
 });
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "password",
+    database: "bamazon_DB"
+  });
+  
+  // connect to the mysql server and sql database
+  connection.connect(function(err) {
+    if (err) throw err;
+    // run the function after the connection is made to prompt the user
+    askForCharacter();
+  });
+  
+ 
+  function askForCharacter(){
+    inquirer.prompt([{
+        type:"list",
+        message:"Select your Character?",
+        name:"Character",
+        choices:["Customer","Manager","Supervisor","Exit"]
+    }]).then(function(response){
+        switch (response.Character){
+            case "Customer":    customerChoices();
+                                break;
+            case "Manager":     managerChoices();
+                                break;
+            case "Supervisor":  supervisorChoices();
+                                break;
+        }
+    });
+}
