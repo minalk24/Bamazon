82c17d7b28ef4d535d0d0160752119deafbdf071
Minal Kharche <minal.berkeley@gmail.com>
2019-06-27 22:13:14 -0700

    updated database with more values

--- database/bamazon.sql
+++ database/bamazon.sql
@@ -13,11 +13,19 @@ create table products(
 );
 
 INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES
-("orange Juice","beverages",10.00,500),
-("cheerios","breakfast & cereal",6.89,100),
-("brownie bites","bakery",12.99,70),
-("chicken broth","canned goods",9.99,140),
-("Cookies","bakery",12.99,70);
+("iphone", "phone", 1000, 200),
+("samsung", "phone", 900, 150),
 
+("macbook", "laptop", 1200, 350),
+("dell", "laptop", 750, 100),
+
+("cannon", "camera", 1100, 200),
+("nikon", "camera", 950, 350),
+
+("LG", "TV", 650, 180),
+("Sony", "TV", 725, 250),
+
+("XBOX", "Gaming Console", 240,150),
+("Playstation","Gaming Console",225,210);
 
 SELECT * FROM products; 
